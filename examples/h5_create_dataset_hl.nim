d7768f33326c3bc55183626b60cbec45b20c9328
Sebastian Schmidt <basti90@gmail.com>
2018-01-05 13:57:17 +0100

    incorporate new vlen and read / write features into example

--- examples/h5_create_dataset_hl.nim
+++ examples/h5_create_dataset_hl.nim
@@ -18,6 +18,7 @@
 ## used to illustrate low level access via C API
 
 import ../src/nimhdf5
+import ../src/hdf5_wrapper
 import ../src/nimhdf5/H5nimtypes
 import typetraits
 import typeinfo
@@ -36,6 +37,10 @@ proc write_some() =
 
   # create dataset
   var dset = h5f.create_dataset("/group1/group2/dset", (2, 2, 5), float64)
+  var dset1D = h5f.create_dataset("/group1/dset1D", 5, float64)
+  var dset_broadcast = h5f.create_dataset("/group1/dsetbroadcast", (3, 3), int)  
+  let vlen_type = special_type(int)
+  var dset_vlen = h5f.create_dataset("/group1/dset_vlen", 5, vlen_type)
 
   # var dat: seq[float64] = newSeq[float64](20)
   # var count = 0
@@ -61,22 +66,56 @@ proc write_some() =
   echo "\n\n\nnew group", g#.groups[]
   echo "old group", t2#.groups[]
 
-  var d_ar = @[ @[ @[1, 2, 3, 4, 5],
-                   @[6, 7, 8, 9, 10] ],
-                @[ @[1, 2, 3, 4, 5],
-                   @[6, 7, 8, 9, 10] ] ]
-  var ddd = mapIt(d_ar, mapIt(it, mapIt(it, float64(it))))
+  var d_ar = @[ @[ @[1'f64, 2, 3, 4, 5],
+                   @[6'f64, 7, 8, 9, 10] ],
+                @[ @[1'f64, 2, 3, 4, 5],
+                   @[6'f64, 7, 8, 9, 10] ] ]
 
-  echo ddd
-  echo ddd.shape, " ", ddd.high
+  var d1d = @[13'f64, 12, 2, 123, 1e9]
+
+  var d_br = @[ @[1, 1, 1],
+                @[1, 1, 1],
+                @[1, 1, 1] ]
+  
+  var d_vlen = @[ @[1, 2, 3],
+                  @[4, 5],
+                  @[6, 7, 8, 9, 10],
+                  @[11, 12, 13, 14, 15],
+                  @[16, 17, 18, 19, 20, 21, 22, 22, 23, 24, 25] ]
   
   # if we simply want to write over the whole dataset 
-  dset[dset.all] = ddd
+  dset[dset.all] = d_ar
+
+  dset1D[dset1D.all] = d1d
+
+  dset_broadcast[dset_broadcast.all] = d_br
+
+  dset_vlen[dset_vlen.all] = d_vlen
+
+  # write values for multiple coordinates by handing sequences of coordinates and
+  # one sequence of the values to write
+  dset.write(@[@[0, 0, 2], @[1, 1, 3]], @[3'f64, 123'f64])
+  # write single value by handing sequence of single coordinate and sequence of single
+  # value
+  dset.write(@[0, 1, 2], @[1337'f64])
+
+  # write whole row by broadcasting one index
+  dset_broadcast.write(0, @[9, 9, 9])
+  # overwrite last column
+  dset_broadcast.write(2, @[7, 7, 7], column = true)
+  
+  # write 2 values into 1D data by handing sequence of indices to write
+  dset1D.write(@[2, 4], @[8'f64, 21e9])
+  # write single value to 1D dataset
+  dset1D.write(0, 299792458'f64)
+  
+  # write single or more elements of VLEN data
+  dset_vlen.write(@[1], @[8, 3, 12, 3, 3, 555, 23234234])
+  # write single element into single index
+  dset_vlen.write(3, 1337)
+  
 
   let dtype = nimToH5type(float64)
-  # status = H5Dwrite(dset.dataset_id, dtype, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-  #                   addr(dat[0]))
-  
 
   # close datasets, groups and file
   status = h5f.close()
@@ -93,6 +132,12 @@ proc read_some() =
   #
   var dataset = file["/group1/group2/dset".dset_str]
   echo dataset
+  # # read some specific elements from the dataset
+  let inds = @[@[0, 0, 0], @[1, 1, 1], @[1, 1, 4]]
+  var data_read = newSeq[float64](3)
+  dataset.read(inds, data_read)
+  echo data_read
+  
   echo file.datasets
   # Note: while we could in principle try to write to the dataset, we
   # just got from the file, this would fail (unfortunately with a libhdf5
