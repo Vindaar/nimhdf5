807a5c984038982c45cafd8834c2f4fcef40d89b
Vindaar <basti90@gmail.com>
2018-08-21 20:36:26 +0200

    clarify argument to H5Zregister to be `ptr H5Z_class2_t`
    Previously was simply `pointer`, but we can do better.

--- src/nimhdf5/wrapper/H5Zpublic.nim
+++ src/nimhdf5/wrapper/H5Zpublic.nim
@@ -15,7 +15,7 @@
 
 ##  Programmer:  Robb Matzke <matzke@llnl.gov>
 ##               Thursday, April 16, 1998
-## 
+##
 
 ##  Public headers needed by this file
 
@@ -33,13 +33,13 @@ when not declared(libname):
     const
       libname* = "libhdf5.so"
 
-## 
+##
 ##  Filter identifiers.  Values 0 through 255 are for filters defined by the
 ##  HDF5 library.  Values 256 through 511 are available for testing new
 ##  filters. Subsequent values should be obtained from the HDF5 development
 ##  team at hdf5dev@ncsa.uiuc.edu.  These values will never change because they
 ##  appear in the HDF5 files.
-## 
+##
 
 type
   H5Z_filter_t* = cint
@@ -69,7 +69,7 @@ const
 ##  filter uses a bit in a 32-bit field,
 ##  so the format would have to be
 ##  changed to accomodate that)
-## 
+##
 ##  Flags for filter definition (stored)
 
 const
@@ -173,33 +173,32 @@ type
     `func`*: H5Z_filter_func_t
     op_data*: pointer
 
-
-## 
+##
 ##  Before a dataset gets created, the "can_apply" callbacks for any filters used
 ##  in the dataset creation property list are called
 ##  with the dataset's dataset creation property list, the dataset's datatype and
 ##  a dataspace describing a chunk (for chunked dataset storage).
-## 
+##
 ##  The "can_apply" callback must determine if the combination of the dataset
 ##  creation property list setting, the datatype and the dataspace represent a
 ##  valid combination to apply this filter to.  For example, some cases of
 ##  invalid combinations may involve the filter not operating correctly on
 ##  certain datatypes (or certain datatype sizes), or certain sizes of the chunk
 ##  dataspace.
-## 
+##
 ##  The "can_apply" callback can be the NULL pointer, in which case, the library
 ##  will assume that it can apply to any combination of dataset creation
 ##  property list values, datatypes and dataspaces.
-## 
+##
 ##  The "can_apply" callback returns positive a valid combination, zero for an
 ##  invalid combination and negative for an error.
-## 
+##
 
 type
   H5Z_can_apply_func_t* = proc (dcpl_id: hid_t; type_id: hid_t; space_id: hid_t): htri_t {.
       cdecl.}
 
-## 
+##
 ##  After the "can_apply" callbacks are checked for new datasets, the "set_local"
 ##  callbacks for any filters used in the dataset creation property list are
 ##  called.  These callbacks receive the dataset's private copy of the dataset
@@ -207,48 +206,48 @@ type
 ##  list passed in to H5Dcreate) and the datatype ID passed in to H5Dcreate
 ##  (which is not copied and should not be modified) and a dataspace describing
 ##  the chunk (for chunked dataset storage) (which should also not be modified).
-## 
+##
 ##  The "set_local" callback must set any parameters that are specific to this
 ##  dataset, based on the combination of the dataset creation property list
 ##  values, the datatype and the dataspace.  For example, some filters perform
 ##  different actions based on different datatypes (or datatype sizes) or
 ##  different number of dimensions or dataspace sizes.
-## 
+##
 ##  The "set_local" callback can be the NULL pointer, in which case, the library
 ##  will assume that there are no dataset-specific settings for this filter.
-## 
+##
 ##  The "set_local" callback must return non-negative on success and negative
 ##  for an error.
-## 
+##
 
 type
   H5Z_set_local_func_t* = proc (dcpl_id: hid_t; type_id: hid_t; space_id: hid_t): herr_t {.
       cdecl.}
 
-## 
+##
 ##  A filter gets definition flags and invocation flags (defined above), the
 ##  client data array and size defined when the filter was added to the
 ##  pipeline, the size in bytes of the data on which to operate, and pointers
 ##  to a buffer and its allocated size.
-## 
+##
 ##  The filter should store the result in the supplied buffer if possible,
 ##  otherwise it can allocate a new buffer, freeing the original.  The
 ##  allocated size of the new buffer should be returned through the BUF_SIZE
 ##  pointer and the new buffer through the BUF pointer.
-## 
+##
 ##  The return value from the filter is the number of bytes in the output
 ##  buffer. If an error occurs then the function should return zero and leave
 ##  all pointer arguments unchanged.
-## 
+##
 
 type
   H5Z_func_t* = proc (flags: cuint; cd_nelmts: csize; cd_values: ptr cuint; nbytes: csize;
                    buf_size: ptr csize; buf: ptr pointer): csize {.cdecl.}
 
-## 
+##
 ##  The filter table maps filter identification numbers to structs that
 ##  contain a pointers to the filter function and timing statistics.
-## 
+##
 
 type
   H5Z_class2_t* = object
@@ -260,10 +259,11 @@ type
     can_apply*: H5Z_can_apply_func_t ##  The "can apply" callback for a filter
     set_local*: H5Z_set_local_func_t ##  The "set local" callback for a filter
     filter*: H5Z_func_t        ##  The actual filter function
-  
 
-proc H5Zregister*(cls: pointer): herr_t {.cdecl, importc: "H5Zregister",
-                                      dynlib: libname.}
+
+proc H5Zregister*(cls: ptr H5Z_class2_t): herr_t {.cdecl, importc: "H5Zregister",
+                                                   dynlib: libname.}
+
 proc H5Zunregister*(id: H5Z_filter_t): herr_t {.cdecl, importc: "H5Zunregister",
     dynlib: libname.}
 proc H5Zfilter_avail*(id: H5Z_filter_t): htri_t {.cdecl, importc: "H5Zfilter_avail",
@@ -271,15 +271,15 @@ proc H5Zfilter_avail*(id: H5Z_filter_t): htri_t {.cdecl, importc: "H5Zfilter_ava
 proc H5Zget_filter_info*(filter: H5Z_filter_t; filter_config_flags: ptr cuint): herr_t {.
     cdecl, importc: "H5Zget_filter_info", dynlib: libname.}
 ##  Symbols defined for compatibility with previous versions of the HDF5 API.
-## 
+##
 ##  Use of these symbols is deprecated.
-## 
+##
 
 when not defined(H5_NO_DEPRECATED_SYMBOLS):
-  ## 
+  ##
   ##  The filter table maps filter identification numbers to structs that
   ##  contain a pointers to the filter function and timing statistics.
-  ## 
+  ##
   type
     H5Z_class1_t* = object
       id*: H5Z_filter_t        ##  Filter ID number
@@ -287,4 +287,3 @@ when not defined(H5_NO_DEPRECATED_SYMBOLS):
       can_apply*: H5Z_can_apply_func_t ##  The "can apply" callback for a filter
       set_local*: H5Z_set_local_func_t ##  The "set local" callback for a filter
       filter*: H5Z_func_t      ##  The actual filter function
-    
