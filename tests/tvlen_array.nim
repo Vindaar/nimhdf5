7e4725b9be4bea7873f68ce16ec259433444cb1d
Vindaar <basti90@gmail.com>
2018-12-16 17:57:35 +0100

    add test cases for single + mulitple vlen data idx read

--- tests/tvlen_array.nim
+++ tests/tvlen_array.nim
@@ -17,25 +17,25 @@ proc create_vlen(h5f: var H5FileObj): H5DataSet =
   result = h5f.create_dataset("/group1/vlen", 4, vlen_type)
   result[result.all] = d_vlen
 
-proc assert_fields(h5f: var H5FileObj, dset: var H5DataSet) =
-  assert(dset.maxshape == dset.shape)
+proc doAssert_fields(h5f: var H5FileObj, dset: var H5DataSet) =
+  doAssert(dset.maxshape == dset.shape)
 
-  assert(dset.dtype == "vlen")
-  
+  doAssert(dset.dtype == "vlen")
+
   # currently if we hand a float64 for a datatype, we end up with
   # akFloat after creation, but when reading it back we get a
   # akFloat64. The first is due to Nim defining float64
-  # 1. as the default float type on a 64 bit machine 
+  # 1. as the default float type on a 64 bit machine
   # 2. in case of float64 actually even more nuanced, in the sense
   #    that Nim defines float as an alias for float64
   let baseKindCheck = if dset.dtypeBaseKind == akFloat or dset.dtypeBaseKind == akFloat64: true else: false
-  assert(baseKindCheck)
+  doAssert(baseKindCheck)
 
-  assert(dset.parent == parentDir(VlenName))
+  doAssert(dset.parent == parentDir(VlenName))
 
-  assert(dset.file == File)
+  doAssert(dset.file == File)
 
-proc assert_data(dset: var H5DataSet) =
+proc doAssert_data(dset: var H5DataSet) =
   let vlen_type = special_type(float)
   let data = dset[vlen_type, float]
 
@@ -43,10 +43,20 @@ proc assert_data(dset: var H5DataSet) =
   # have to flatten since do not return the data in
   # the shape of the array we write, but rather as
   # a flattened array, i.e.
-  # assert(data.shape == d_vlen.shape)
+  # doAssert(data.shape == d_vlen.shape)
   # would fail
   for i in 0 ..< data.len:
-    assert(data[i] == d_vlen[i])
+    doAssert(data[i] == d_vlen[i])
+
+  # read single value and compare
+  for i in 0 ..< dset.shape[0]:
+    let d = dset[vlen_type, float, i]
+    doAssert(d == d_vlen[i])
+
+  # read several values and compare
+  let d03 = dset[vlen_type, float, @[0, 3]]
+  doAssert d03[0] == d_vlen[0]
+  doAssert d03[1] == d_vlen[3]
 
 when isMainModule:
   # open file, create dataset
@@ -54,23 +64,22 @@ when isMainModule:
     h5f = H5File(File, "rw")
     dset = h5f.create_vlen()
   # perform 1st checks on still open file
-  h5f.assert_fields(dset)
+  h5f.doAssert_fields(dset)
   # close and reopen
   var err = h5f.close()
-  assert(err >= 0)
+  doAssert(err >= 0)
   var
     h5f_read = H5File(File, "r")
   # get same dset from before
   dset = h5f_read[VlenName.dset_str]
-  # check if assertions still hold true (did we read correctly?)
-  h5f_read.assert_fields(dset)
+  # check if doAssertions still hold true (did we read correctly?)
+  h5f_read.doAssert_fields(dset)
 
   # now read actual data and compare with what we wrote to file
-  dset.assert_data()
-  
+  dset.doAssert_data()
+
   err = h5f_read.close()
-  assert(err >= 0)
-  
+  doAssert(err >= 0)
+
   # clean up after ourselves
   removeFile(File)
-  
