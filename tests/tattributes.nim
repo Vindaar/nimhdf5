552475303cdaa97f25cd84f0ab2785335d032ebb
Vindaar <basti90@gmail.com>
2019-02-17 22:00:48 +0100

    add test case checking for overwriting w/o closing file

--- tests/tattributes.nim
+++ tests/tattributes.nim
@@ -8,17 +8,17 @@ const
   GrpName = "group1"
   # group to which we copy attributes
   GrpCopy = "groupCopy"
-  TimeStr = "21:19"     
-  Counter = 128       
+  TimeStr = "21:19"
+  Counter = 128
   SeqAttr = @[1, 2, 3, 4]
 
 proc write_attrs(grp: var H5Group) =
-  
+
   # # now write some attributes
   grp.attrs["Time"] = TimeStr
   grp.attrs["Counter"] = Counter
   grp.attrs["Seq"] = SeqAttr
-  
+
 proc assert_attrs(grp: var H5Group) =
 
   assert(grp.attrs["Time", string] == TimeStr)
@@ -42,7 +42,32 @@ proc assert_delete(grp: var H5Group) =
   assert(grp.deleteAttribute("Counter"))
   assert(grp.attrs.num_attrs == 1)
   assert(grp.deleteAttribute("Seq"))
-  assert(grp.attrs.num_attrs == 0)  
+  assert(grp.attrs.num_attrs == 0)
+
+proc assert_overwrite(grp: var H5Group) =
+  var mcounter = Counter
+
+  # grp.file_ref[].printOpenObjects()
+  # var ids = getOpenObjectIds(grp.file_ref[], okAttr)
+  # for id in ids:
+  #   let openN = getAttrName(id)
+  #   echo id.close(okAttr), " was ", openN
+
+  grp.attrs["Counter"] = mcounter
+  doAssert(grp.attrs["Counter", int] == mcounter)
+  inc mcounter
+
+  # ids = getOpenObjectIds(grp.file_ref[], okAttr)
+  # for id in ids:
+  #   let openN = getAttrName(id)
+  #   echo id.close(okAttr), " was ", openN
+  # grp.file_ref[].printOpenObjects()
+
+  grp.attrs["Counter"] = mcounter
+  doAssert(grp.attrs["Counter", int] == mcounter)
+  inc mcounter
+  grp.attrs["Counter"] = mcounter
+  doAssert(grp.attrs["Counter", int] == mcounter)
 
 when isMainModule:
 
@@ -55,6 +80,7 @@ when isMainModule:
   grp.write_attrs
   grp.assert_attrs
 
+
   # copy attributes of grp to grpCp
   grpCp.copy_attributes(grp.attrs)
   # now simply assert these attributes in the same way
@@ -66,7 +92,7 @@ when isMainModule:
   # open again, again with write access to delete attributes again
   h5f = H5File(File, "rw")
   grp = h5f[GrpName.grp_str]
-  grpCp = h5f[GrpCopy.grp_str]  
+  grpCp = h5f[GrpCopy.grp_str]
   # and check again
   grp.assert_attrs
   grpCp.assert_attrs
@@ -75,8 +101,10 @@ when isMainModule:
   grp.assert_delete
   grpCp.assert_delete
 
+  grp.assert_overwrite
+
   err = h5f.close()
   assert(err >= 0)
-  
-  # clean up after ourselves  
+
+  # clean up after ourselves
   removeFile(File)
