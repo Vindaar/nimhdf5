a53dec2b5d7d7d73b816dd191ef919a6c2ac5dc4
Vindaar <basti90@gmail.com>
2019-11-17 18:11:30 +0100

    add to JSON conversion for attributes
    For attributes the user often probably does not care too much about
    the underlying type. In these cases just being able to quickly get all
    attributes to look at / iterate over via JSON seems useful (it is for
    me anyways).

--- src/nimhdf5/h5util.nim
+++ src/nimhdf5/h5util.nim
@@ -17,6 +17,29 @@ import H5nimtypes
 import util
 import datatypes
 
+import attributes
+
+import json
+func `%`(c: char): JsonNode = % $c # for `withAttr` returning a char
+iterator attrsJson*(attrs: H5Attributes): (string, JsonNode) =
+  ## yields all attribute keys and their values as `JsonNode`. This way
+  ## we can actually return all values to the user with one iterator.
+  ## And for attributes the variant object overhead does not matter anyways.
+  attrs.read_all_attributes
+  for key, attr in pairs(attrs.attr_tab):
+    attrs.withAttr(key):
+      yield (key, % attr)
+    discard attr.close()
+
+iterator attrsJson*[T: H5FileObj | H5Group | H5DataSet](h5o: T): (string, JsonNode) =
+  for key, val in attrsJson(h5o.attrs):
+    yield (key, val)
+
+proc attrsToJson*[T: H5Group | H5DataSet](h5o: T): JsonNode =
+  ## returns all attributes as a json node of kind `JObject`
+  result = newJObject()
+  for key, jval in h5o.attrsJson:
+    result[key] = jval
 
 proc pretty*(dset: H5DataSet, indent = 0, full = false): string =
   result = repeat(' ', indent) & "{\n"
