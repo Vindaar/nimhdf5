40dbe8498124abb9eea431973b15ed7dc8ea33c7
Vindaar <basti90@gmail.com>
2021-12-17 16:48:38 +0100

    [tests] add test for overwriting & deleting dataset in root
    At this commit this test produces a segfault. The code is fixed in a
    later commit (to shrink the next commits at least a tad).

--- tests/toverwrite.nim
+++ tests/toverwrite.nim
@@ -7,6 +7,7 @@ import typeinfo
 const
   File = "tests/dset.h5"
   DsetName = "/group1/dset"
+  DsetRootName = "/dset"
 var d_ar = @[ @[1'f64, 2, 3, 4, 5],
               @[6'f64, 7, 8, 9, 10] ]
 var d_new = @[ @[1'f64, 2, 3, 4, 5],
@@ -18,17 +19,24 @@ when isMainModule:
   var
     h5f = H5File(File, "rw")
 
-  var dset = h5f.create_dataset("/group1/dset", (2, 5), float64)
-  dset[dset.all] = d_ar
-  doAssert dset.shape == @[2, 5]
-  dset = h5f.create_dataset("/group1/dset", (3, 5), float64, overwrite = true)
-  dset[dset.all] = d_new
-  doAssert dset.shape == @[3, 5]
-  # check dataset still exists
-  doAssert DsetName in h5f
-  discard h5f.delete(DsetName)
-  # check it doesn't exist anymore
-  doAssert (not (DsetName in h5f))
+  template createAndOverwrite(path: string): untyped =
+    var dset = h5f.create_dataset(path, (2, 5), float64)
+    dset[dset.all] = d_ar
+    doAssert dset.shape == @[2, 5]
+    dset = h5f.create_dataset(path, (3, 5), float64, overwrite = true)
+    dset[dset.all] = d_new
+    doAssert dset.shape == @[3, 5]
+
+    # check dataset still exists
+    doAssert path in h5f
+    discard h5f.delete(path)
+    ## check it doesn't exist anymore
+    doAssert (not (path in h5f))
+
+
+  createAndOverwrite(DsetName)
+  createAndOverwrite(DsetRootName)
+
   let err = h5f.close()
   doAssert(err >= 0)
 
