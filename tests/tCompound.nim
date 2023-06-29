8f324481348339e2ae77ab269b737202ae5363da
Vindaar <basti90@gmail.com>
2023-06-29 20:49:02 +0200

    [tests] add test cases for more compound object types

--- tests/tCompound.nim
+++ tests/tCompound.nim
@@ -4,12 +4,14 @@ const
   File = "tests/tCompound.h5"
   Dset = "DComp"
   DsetTup = "DCompTup"
+  DsetAnTup = "DCompAnTup"
 
 type
   Comp = object
     a: float
+    c: float32 ## having float32 here tests that Nim's alignment is correctly taken care of
+               ## (another 4 byte after the float32)
     b: int
-    c: float32
 
   Tup = tuple
     d: float
@@ -25,19 +27,32 @@ proc assert_dataset[T](h5f: H5File, data: seq[T], name: string) =
   let readData = dset[T]
   doAssert readData == data
 
+import typetraits
 when isMainModule:
   var data = newSeq[Comp](10)
   var dataTup = newSeq[Tup](10)
+  var dataAnTup = newSeq[(float, float32, int)](10) ## anonymous tuple to check we get correct alignment for these
   for i in 0 ..< 10:
-    data[i] = Comp(a: i.float * 1.1, b: i, c: i.float32 / 1.111)
+    let x = Comp(a: i.float * 1.1, b: i, c: i.float32 / 1.111'f32)
+    #echo sizeof(x)
+    data[i] = x
+    let y = (i.float * 1.1, i.float32 / 1.111'f32, i)
+    dataAnTup[i] = y
+    #echo sizeof(y)
+    #echo sizeof(y[0]), " addr ", cast[uint](addr(y[0]))
+    #echo sizeof(y[1]), " addr ", cast[uint](addr(y[1]))
+    #echo sizeof(y[2]), " addr ", cast[uint](addr(y[2]))
     dataTup[i] = (d: i.float * 2.5, e: i * 2, f: i * 3)
+
 
   var h5f = H5open(File, "rw")
 
   h5f.write_dataset(data, Dset)
   h5f.write_dataset(dataTup, DsetTup)
+  h5f.write_dataset(dataAnTup, DsetAnTup)
   h5f.assert_dataset(data, Dset)
   h5f.assert_dataset(dataTup, DsetTup)
+  h5f.assert_dataset(dataAnTup, DsetAnTup)
 
   var err = h5f.close()
   assert err >= 0
@@ -45,6 +60,7 @@ when isMainModule:
   h5f = H5open(File, "r")
   h5f.assert_dataset(data, Dset)
   h5f.assert_dataset(dataTup, DsetTup)
+  h5f.assert_dataset(dataAnTup, DsetAnTup)
 
   err = h5f.close()
   assert err >= 0
---
+++ tests/tCompoundWithSeq.nim
@@ -0,0 +1,111 @@
+import tables, nimhdf5
+
+proc writeDeserTest[T](x: T, f: string) =
+  x.toH5(f)
+
+  let xx = deserializeH5[T](f)
+  echo "Deserialized: ", xx
+
+  ## Verify they are the same!
+  doAssert xx == x
+
+block Float:
+  let file = "/tmp/cacheTab_float.h5"
+  type
+    TabKey = (int, string)
+    TabVal = float
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = 123.54
+
+  writeDeserTest(cacheTab, file)
+
+block String:
+  let file = "/tmp/cacheTab_string.h5"
+  type
+    TabKey = (int, string)
+    TabVal = string
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = "HALO"
+
+  writeDeserTest(cacheTab, file)
+
+block SeqInt:
+  let file = "/tmp/cacheTab_seqint.h5"
+  type
+    TabKey = (int, string)
+    TabVal = seq[int]
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = @[1, 2, 3, 4, 5] #@[1.1, 1.12, 123.54]
+
+  writeDeserTest(cacheTab, file)
+
+block SeqFloat:
+  let file = "/tmp/cacheTab_seqfloat.h5"
+  type
+    TabKey = (int, string)
+    TabVal = seq[float]
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = @[1.1, 1.12, 123.54]
+
+  writeDeserTest(cacheTab, file)
+
+block SeqString:
+  let file = "/tmp/cacheTab_seqstring.h5"
+  type
+    TabKey = (int, string)
+    TabVal = seq[string]
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = @["A", "HALO"]
+
+  writeDeserTest(cacheTab, file)
+
+block SeqTuple:
+  let file = "/tmp/cacheTab_seqtuple.h5"
+  type
+    TabKey = (int, string)
+    TabVal = seq[(int, float)]
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = @[(1, 1.2), (5, 123.4)]
+
+  writeDeserTest(cacheTab, file)
+
+block SeqTupleStr:
+  let file = "/tmp/cacheTab_seqtuple_str.h5"
+  type
+    TabKey = (int, string)
+    TabVal = seq[(string, float)]
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = @[("A", 1.2), ("HALO", 123.4)]
+
+  writeDeserTest(cacheTab, file)
+
+block SeqFloatLarge:
+  let file = "/tmp/cacheTab_seqfloat_large.h5"
+  type
+    TabKey = (int, string)
+    TabVal = seq[float]
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal]()
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = @[1.2, 3.21, 543.4]
+
+  writeDeserTest(cacheTab, file)
+
+block TrickyTuples:
+  let file = "/tmp/cacheTab_tricky_tuples.h5"
+  ## This checks whether we handle alignment within a tuple that does not need to be
+  ## converted, but is contained in an outer tuple that is converted.
+  type
+    TabKey = (int, string)
+    TabVal = (int, float32, float)
+    CacheTabTyp = Table[TabKey, TabVal]
+  var cacheTab = initTable[TabKey, TabVal](1)
+  cacheTab[(232, "DEGA5e09EEGNAGEN")] = (4, 1.2'f32, 53.2)
+
+  writeDeserTest(cacheTab, file)
